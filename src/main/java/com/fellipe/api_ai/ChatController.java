31ae0031fac18d17545550c60be706b47cab8157
fellipe10 <fellipef098@gmail.com>
2025-07-02 14:39:05 -0300

    Commit do spring AI + angular - parte 1

--- src/main/java/com/fellipe/api_ai/ChatController.java
+++ src/main/java/com/fellipe/api_ai/ChatController.java
@@ -1,13 +1,8 @@
 package com.fellipe.api_ai;
 
-
-
-
-
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,8 +33,5 @@ public class ChatController {
                               .call()
                               .content();
     }
-   
-    
-    
     
 }
